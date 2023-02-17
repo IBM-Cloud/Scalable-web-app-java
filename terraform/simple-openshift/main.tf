82b8eb574a57c02ec78d1d89fd9989fd1a7ce151
Chuck Cox <chuckc@us.ibm.com>
2023-02-17 13:57:37 -0600

    chore: add required_providers

--- terraform/simple-openshift/main.tf
+++ terraform/simple-openshift/main.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = ">= 0.12"
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = ">= 1.12.0"
+    }
+  }
 }
 
 resource "random_string" "random" {
